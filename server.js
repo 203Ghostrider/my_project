105e99e23fcbaee785df711e78225c79ac4c7621
test <test@HIROYUKInoMacBook-Air.local>
2025-01-04 15:58:51 +0900

    Changed default port to 3000

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path'); // 追加
 
 // Expressアプリケーションのインスタンスを作成
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
